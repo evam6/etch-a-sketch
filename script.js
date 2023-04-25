fc66a34e7b44ef1b9f20633163fc6fa728d30b84
eva <evamatejkova.e@gmail.com>
2023-04-25 13:18:00 +0200

    Functionality extended - added buttons for color modes and clearing the grid

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
 const divGrid = document.querySelector('.grid');
 let gridSize = 16;
+let colorMode = 'monochrome';
 
 const buttonSize = document.querySelector('.button-size');
+const buttonMono = document.querySelector('.button-mono');
+const buttonRGB = document.querySelector('.button-rgb');
+const buttonClear = document.querySelector('.button-clear');
+
+// getting user input for grid size and creating such grid
 buttonSize.addEventListener('click', () => {
-    gridSize = prompt('Enter number of squares per side:');
-    while (gridSize > 100) {
-        gridSize = prompt('Maximum limit is 100. Please choose smaller value:')
+    gridSize = prompt('Enter number of squares per side (from 2 to 100):');
+    while (gridSize > 100 || gridSize < 2) {
+        gridSize = prompt('Limits: minimum 2, maximum 100. Please choose different value:')
     }
     deleteGrid();
     createGrid(gridSize);
+    setColor(colorMode);
+})
+
+// clearing the grid
+buttonClear.addEventListener('click', () => {
+    deleteGrid();
+    createGrid(gridSize);
+    setColor(colorMode);
+}) 
+
+buttonMono.addEventListener('click', () => {
+    colorMode = 'monochrome';
+    setColor(colorMode);
+})
+
+buttonRGB.addEventListener('click', () => {
+    colorMode = 'rgb';
+    setColor(colorMode);
 })
 
 function createGrid(gridSize) {
     divGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     divGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
-    for (i = 1; i <= gridSize*gridSize; i++) {
+    for (let i = 1; i <= gridSize * gridSize; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('element');
-        gridElement.addEventListener('mouseenter', () => {
-        gridElement.style.backgroundColor = 'grey';
-        })
         divGrid.appendChild(gridElement);
     }
     return;
 }
 
+function setColor(colorMode) {
+    const elements = document.querySelectorAll('.element');
+    elements.forEach((element) => {
+        element.addEventListener('mouseenter', () => {
+            if (colorMode == 'rgb') {
+                const letters = '0123456789ABCDEF';
+                let colorRGB = '#';
+                for (let i = 0; i < 6; i++) {
+                    colorRGB += letters[Math.floor(Math.random() * 16)];
+                }
+                color = colorRGB;
+            } else {
+                color = 'grey';
+            }
+            element.style.backgroundColor = color;
+            
+        })
+    })
+}
+
+// deleting grid by removing all the grid elements 
 function deleteGrid() {
     const elements = document.querySelectorAll('.element');
     elements.forEach(element => {
@@ -34,3 +75,4 @@ function deleteGrid() {
 }
 
 createGrid(gridSize);
+setColor(colorMode);
